e023eaeac6b7798e46b480b90bb1ebab9fc80afe
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-04-08 22:58:27 +0530

    feat: UI for registering a user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
-export default function Home() {
+import { TextField, Button, Input, Label, Card } from "@heroui/react"
+
+import { register } from "@/actions/register"
+
+export default function HomePage() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Card>
+        <Card.Header>
+          <Card.Title>Register</Card.Title>
+          <Card.Description>Add new users to the database</Card.Description>
+        </Card.Header>
+
+        <form className="grid gap-4" action={register}>
+          <TextField
+            className="w-full max-w-64"
+            variant="secondary"
+            autoComplete="name"
+            name="name"
+          >
+            <Label>Name</Label>
+            <Input placeholder="John Doe" />
+          </TextField>
+
+          <TextField
+            className="w-full max-w-64"
+            autoComplete="email"
+            variant="secondary"
+            name="email"
+            type="email"
+          >
+            <Label>Email</Label>
+            <Input placeholder="johndoe@email.com" />
+          </TextField>
+          <Button type="submit">Submit</Button>
+        </form>
+      </Card>
     </main>
   )
 }
