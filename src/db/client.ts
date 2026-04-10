678a09ef57f89d330b70680efce2bd69c5d28dd8
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-04-10 23:00:30 +0530

    feat: Conditionally use Prisma adapter

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,9 +1,14 @@
+import { PrismaNeon } from "@prisma/adapter-neon"
 import { PrismaPg } from "@prisma/adapter-pg"
 
 import { PrismaClient, Prisma } from "@/db/generated/prisma/client"
 import env from "@/lib/env"
 
-const adapter = new PrismaPg({ connectionString: env.DATABASE_URL })
+const adapter =
+  process.env.NODE_ENV === "production"
+    ? new PrismaNeon({ connectionString: env.DATABASE_URL })
+    : new PrismaPg({ connectionString: env.DATABASE_URL })
+
 const prisma = new PrismaClient({ adapter })
 
 export { prisma, Prisma }
